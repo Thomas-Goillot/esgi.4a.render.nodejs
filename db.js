b9a826a73ad70323721fa757b5acaf78f4f3d628
Thomas Goillot <thomadgllt@gmail.com>
2024-10-30 14:56:50 +0100

    .env

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-// Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  process.env.DB_NAME, // Nom de la base de données
+  process.env.DB_USER, // Nom d'utilisateur
+  process.env.DB_PASSWORD, // Mot de passe
   {
-    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
     dialectOptions: {
       ssl: {
         require: true,
@@ -12,13 +15,17 @@ const sequelize = new Sequelize(
       },
     },
     define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
-    }
-  },
-)
+      createdAt: "added",
+      updatedAt: "updated",
+    },
+  }
+);
 
-sequelize.authenticate()
-sequelize.sync()
+sequelize
+  .authenticate()
+  .then(() => console.log("Connexion établie avec succès."))
+  .catch((err) => console.error("Erreur de connexion :", err));
 
-module.exports = sequelize
+sequelize.sync();
+
+module.exports = sequelize;
